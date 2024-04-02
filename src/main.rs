09bdc726f727f4f33aeb416f1898ba2d289fecd7
kangseokgyu <ksg890528@gmail.com>
2024-04-02 21:47:40 +0900

    stderr 출력 추가

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Failed to parse arguments: {err}");
+        eprintln!("Failed to parse arguments: {err}");
         process::exit(1);
     });
 
     if let Err(err) = run(config) {
-        println!("Application error: {err}");
+        eprintln!("Application error: {err}");
         process::exit(1);
     }
 }
